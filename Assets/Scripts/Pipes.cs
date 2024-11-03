5b11bca1cc4f1b733702e73498fc097aeaffa649
JES0406 <112687530+JES0406@users.noreply.github.com>
2024-11-03 14:17:49 +0100

    Initialation and player movement

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    [Header("Dependencies")]
+    public Transform top;
+    public Transform bottom;
+    public float pipeSpeed = 3f;
+
+    [Header("Parameters")]
+
+    private float leftEdge;
+
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+
+    private void Update()
+    {
+         transform.position += Vector3.left * pipeSpeed * Time.deltaTime;
+    }
+
+}
